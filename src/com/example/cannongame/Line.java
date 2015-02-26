ea214edd02d56a76b22db0f2eeb1af64beaa61b5
Sndeep silumula <sandeep.silumula@gmail.com>
2015-02-26 18:57:58 -0500

    CannonGame

---
+++ src/com/example/cannongame/Line.java
@@ -0,0 +1,11 @@
+// Line.java
+// Class Line represents a line with two endpoints.
+package com.example.cannongame;
+
+import android.graphics.Point;
+
+public class Line
+{
+   public Point start = new Point(); // start Point--(0,0) by default
+   public Point end = new Point(); // end Point--(0,0) by default
+} // end class Line
